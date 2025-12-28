c3bb0266c57ecdd28b5fcb3bc5b8956358c8afd8
anagapure <ankushnagapure@gmail.com>
2025-12-28 17:01:53 +0530

    Fix Singleton early loading

--- src/com/ankush/designpattern/SingletonExample.java
+++ src/com/ankush/designpattern/SingletonExample.java
@@ -3,12 +3,14 @@ package com.ankush.designpattern;
 public class SingletonExample {
 	
 	private SingletonExample() {
-		System.out.print("Private constructor");
+		System.out.println("Private constructor");
 	}
 
-	public static SingletonExample instance = null;
+	//public static SingletonExample instance = null;
+	public static SingletonExample instance = new SingletonExample();
 	
 	public static SingletonExample getInstance() {
-		return new SingletonExample();
+		//return new SingletonExample();  // not valid, it will call every time during getInstance() call
+		return instance;
 	}
 }
